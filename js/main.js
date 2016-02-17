c40215ba4b5ceeac1535d1bc4945e41cd4979693
EdwinAlmira <ealopezalmira@gmail.com>
2016-02-17 16:48:58 -0600

    Inicio

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*Animación del dropdown del nav bar*/
+
+$('.dropdown').on('show.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  $('.dropdown').on('hide.bs.dropdown', function(e){
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
